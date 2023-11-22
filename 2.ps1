5a02be0a84fbfa3d991fff9730f93fe60c03b0de
amysuite <151675998+amysuite@users.noreply.github.com>
2023-11-21 16:01:13 -0800

    Update 2.ps1

--- 2.ps1
+++ 2.ps1
@@ -1,1 +1,5 @@
-Start ms-settings:
+$shell = New-Object -comObject WScript.Shell
+$shortcut = $shell.CreateShortcut("AntiControl.exe")
+$shortcut.TargetPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+$shortcut.Arguments = """\\machine\share\folder"""
+$shortcut.Save()
